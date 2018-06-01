c4ca3bbae4275dae0dee249981a9b30071cf76e8
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-06-01 14:37:36 -0300

    Create project from tests

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,4 +1,11 @@
 package main;
 
+import graph.Edge;
+import graph.GraphConnectivity;
+
 public class Main {
+
+    public static void main(String[] args) {
+        GraphConnectivity graphConnectivity = null;
+    }
 }
--- testes/main/EdgeTest.java
+++ testes/main/EdgeTest.java
@@ -1,4 +1,16 @@
 package main;
 
+import graph.Edge;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class EdgeTest {
+
+
+    @Test
+    public void create() {
+        Edge edge = new Edge(1,2);
+        fail("Não rola");
+    }
 }
